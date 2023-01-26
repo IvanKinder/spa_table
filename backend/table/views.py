a4bf48a0d1050d8265834bf46c0bba7d5a079be8
Ivan Kinder <kinder_iv@magnit.ru>
2023-01-26 16:53:32 +0300

    Добавлена пагинация ( остались незначительные косяки с отключением кнопок предыдущая/следующая )

--- backend/table/views.py
+++ backend/table/views.py
@@ -1,7 +1,7 @@
 import json
+import math
 
 from django.contrib.auth.models import User
-from django.core import serializers
 from django.db import transaction, IntegrityError
 from django.http import JsonResponse
 from django.views import View
@@ -11,12 +11,14 @@ from table.models import Appeal
 
 class AppealsView(View):
     def get(self, request, *args, **kwargs):
+        pages_count = 0
         params = request.GET
 
         filter_by = params.get('filterBy').split('-')[0] if params.get('filterBy') else ''
         filter_by_eq = params.get('filterBy').split('-')[1] if params.get('filterBy') else ''
         order_by = params.get('orderBy') if params.get('orderBy') else ''
         search_num = params.get('search')
+        page = params.get('page')
 
         try:
             data = Appeal.objects.all()
@@ -27,23 +29,29 @@ class AppealsView(View):
             if order_by:
                 data = data.order_by(order_by)
 
-            if search_num:
+            if search_num and search_num != 'null':
                 data = data.filter(number__contains=search_num)
 
-            data = serializers.serialize('json', data)
+            pages_count = math.ceil(data.count() / 10)
+
+            data = data[(int(page)-1)*10:int(page)*10]
+
+            data = [{'creator': User.objects.get(pk=value['creator_id']).username, **value} for value in
+                    list(data.values())]
 
         except Exception as e:
-            data = '[]'
+            data = str(e)
 
         return JsonResponse({
-            'data': json.loads(data),
+            'data': data,
+            'pagesCount': pages_count
         })
 
     def post(self, request, *args, **kwargs):
         body = json.loads(request.body)
         appeal_number = body.get('appealNumber')
         description = body.get('description')
-        data = '[]'
+        data = []
 
         if appeal_number and description:
             try:
@@ -51,12 +59,11 @@ class AppealsView(View):
                     new_appeal = Appeal(number=appeal_number, description=description, creator=User.objects.get(pk=1))
                     new_appeal.save()
                     data = str(new_appeal.number)
-                    print(data)
             except IntegrityError as e:
-                data = '[]'
+                data = str(e)
             except Exception as e:
-                data = '[]'
+                data = str(e)
 
         return JsonResponse({
-            'data': json.loads(data),
+            'data': data
         })
--- frontend/src/components/AppealsTable.vue
+++ frontend/src/components/AppealsTable.vue
@@ -3,7 +3,7 @@
     <ModalWindow ref="modal"></ModalWindow>
     <p>Список Обращений</p>
     <div class="top-bar">
-      <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+      <div class="btn-group" role="group" aria-label="Button group">
         <button type="button" class="btn btn-light first-btn" @click="getAppeals({
           filterBy: 'status-0'
         })">Открытые
@@ -116,28 +116,53 @@
         </div>
       </div>
     </div>
-    <div class="table">
+    <div class="table" id="table">
       <div
           class="row"
           v-for="appeal in appealsList"
-          :key="appeal.fields.id">
+          :key="appeal.id">
         <div class="col">
-          {{ appeal.fields.number }}
+          {{ appeal.number }}
         </div>
         <div class="col">
-          {{ appeal.fields.created_at }}
+          {{ appeal.created_at }}
         </div>
         <div class="col">
-          {{ appeal.fields.description }}
+          {{ appeal.description }}
         </div>
         <div class="col">
-          {{ appeal.fields.creator }}
+          {{ appeal.creator }}
         </div>
         <div class="col">
-          {{ appeal.fields.status }}
+          {{ appeal.status }}
         </div>
       </div>
     </div>
+    <nav aria-label="Page navigation">
+      <ul class="pagination">
+        <li class="page-item">
+          <button type="button"
+                  class="btn disabled"
+                  @click="setPage(page - 1)"
+                  id="prevBtn">&laquo;
+          </button>
+        </li>
+        <li v-for="page in pages" :key="page">
+          <button type="button"
+                  class="btn"
+                  @click="setPage(page)"
+                  :id="page">{{ page }}
+          </button>
+        </li>
+        <li class="page-item">
+          <button type="button"
+                  class="btn"
+                  @click="setPage(page + 1)"
+                  id="nextBtn">&raquo;
+          </button>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -153,40 +178,82 @@ export default {
     return {
       appealsList: [],
       filterBy: '',
-      orderBy: ''
+      orderBy: '',
+      pagesCount: 0,
+      page: 1,
+      searchLike: null,
     }
   },
   methods: {
     getAppeals(params) {
-      this.$refs.searchField.value = '';
-
       if (Object.hasOwn(params, 'orderBy')) {
         this.orderBy = params.orderBy;
+        this.$refs.searchField.value = '';
+        this.searchLike = null;
       }
       if (Object.hasOwn(params, 'filterBy')) {
         this.filterBy = params.filterBy;
+        this.$refs.searchField.value = '';
+        this.searchLike = null;
       }
-      fetch(`http://127.0.0.1:8000/appeals/?filterBy=${this.filterBy}&orderBy=${this.orderBy}`).then((response) => {
+      fetch(`http://127.0.0.1:8000/appeals/?filterBy=${this.filterBy}&orderBy=${this.orderBy}&page=${this.page}&search=${this.searchLike}`).then((response) => {
         return response.json();
       }).then((data) => {
         this.appealsList = data.data;
+        this.pagesCount = data.pagesCount;
       });
     },
     searchByNumber(e) {
-      fetch(`http://127.0.0.1:8000/appeals/?search=${e.target.value}&filterBy=${this.filterBy}&orderBy=${this.orderBy}`).then((response) => {
-        return response.json();
-      }).then((data) => {
-        this.appealsList = data.data;
-      });
+      this.searchLike = e.target.value;
+      this.getAppeals({});
     },
     onAddClick() {
       this.$refs.modal.modalVisible = true;
       this.$refs.modal.parentComponent = this;
     },
+    setPage(page) {
+      const prevBtn = document.getElementById('prevBtn');
+      const nextBtn = document.getElementById('nextBtn');
+      const table = document.getElementById('table');
+
+      table.scrollTo(0, 0);
+
+      if (document.getElementById(this.page)) {
+        document.getElementById(this.page).classList.remove('disabled');
+      }
+
+      if (page === 1) {
+        prevBtn.classList.add('disabled');
+      } else {
+        prevBtn.classList.remove('disabled');
+      }
+      if (page === this.pagesCount || this.pagesCount === 1) {
+        nextBtn.classList.add('disabled');
+      } else {
+        nextBtn.classList.remove('disabled');
+      }
+      this.page = page;
+
+      if (document.getElementById(this.page)) {
+        document.getElementById(this.page).classList.add('disabled');
+      }
+      this.getAppeals({});
+    },
+  },
+  computed: {
+    pages() {
+      const pagesList = [];
+
+      for (let i = 1; i < this.pagesCount + 1; i++) {
+        pagesList.push(i);
+      }
+
+      return pagesList
+    }
   },
   mounted() {
     this.getAppeals({});
-  }
+  },
 }
 </script>
 
@@ -202,7 +269,7 @@ p {
 .table {
   border: 1px solid #e0e0e0;
   border-radius: 8px;
-  max-height: 30vh;
+  height: 30vh;
   overflow: auto;
 }
 
@@ -227,5 +294,13 @@ p {
   margin-top: 2vh;
   display: flex;
   justify-content: space-between;
+}
+
+.pagination {
+  display: inline-flex;
+}
+
+.page-link {
+  color: black
 }
 </style>
